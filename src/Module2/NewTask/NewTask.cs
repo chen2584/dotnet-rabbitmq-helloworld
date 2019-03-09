a691e0a35d410f92bec5dcb29fc952cabbba37d0
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2019-03-09 23:57:10 +0700

    add module3

--- src/Module2/NewTask/NewTask.cs
+++ src/Module2/NewTask/NewTask.cs
@@ -12,7 +12,7 @@ namespace NewTask
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "task_queue2", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
                 string message = GetMessage(args);
                 var body = Encoding.UTF8.GetBytes(message);
@@ -20,7 +20,7 @@ namespace NewTask
                 var properties = channel.CreateBasicProperties();
                 properties.Persistent = true;
 
-                channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+                channel.BasicPublish(exchange: "", routingKey: "task_queue2", basicProperties: properties, body: body);
                 Console.WriteLine(" [x] Sent {0}", message);
             }
 
--- src/Module2/Worker/Worker.cs
+++ src/Module2/Worker/Worker.cs
@@ -14,7 +14,7 @@ namespace RabbitMQ.Send
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "task_queue2", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
                 channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
@@ -32,7 +32,7 @@ namespace RabbitMQ.Send
 
                     channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                 };
-                channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+                channel.BasicConsume(queue: "task_queue2", autoAck: false, consumer: consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
