ae109e4a7d174f581921d085906792340bf39a1b
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2019-03-30 15:24:54 +0700

    add module4

---
+++ src/Module4/ReceiveLogsDirect/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogsDirect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "user", Password = "password" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                                            Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var serverity in args)
+                {
+                    channel.QueueBind(queue: queueName,
+                                  exchange: "direct_logs",
+                                  routingKey: serverity);
+                }
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                      routingKey, message);
+                };
+                channel.BasicConsume(queue: "Chen",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
